e100cd4846e4cecbca39bca5273510c49b73a29c
Bryan Simkins <bryan.simkins@transparent.partners>
2026-01-15 20:25:17 -0500

    Fix crypto randomUUID shim typing

--- web/src/components/CryptoUUIDShim.tsx
+++ web/src/components/CryptoUUIDShim.tsx
@@ -3,7 +3,8 @@
 import { v4 as uuidv4 } from "uuid";
 
 if (typeof crypto !== "undefined" && !crypto.randomUUID) {
-  (crypto as Crypto & { randomUUID?: () => string }).randomUUID = () => uuidv4();
+  (crypto as Crypto & { randomUUID?: () => `${string}-${string}-${string}-${string}-${string}` }).randomUUID = () =>
+    uuidv4() as `${string}-${string}-${string}-${string}-${string}`;
 }
 
 export function CryptoUUIDShim() {
